51a20802f360c3e186c6d5135d0c895cbfb61beb
tbasic <tbasicapp@gmail.com>
2020-05-17 10:30:02 +0900

    step1

---
+++ src/main/java/net/myslipp/MyslippApplication.java
@@ -0,0 +1,13 @@
+package net.myslipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyslippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyslippApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/myslipp/MyslippApplicationTests.java
@@ -0,0 +1,13 @@
+package net.myslipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyslippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
