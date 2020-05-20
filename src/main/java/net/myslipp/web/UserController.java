e19514cd732d14c0f7e70611e3120702a4e17548
tbasic <tbasicapp@gmail.com>
2020-05-20 13:20:47 +0900

    step5

---
+++ src/main/java/net/myslipp/web/UserController.java
@@ -0,0 +1,35 @@
+package net.myslipp.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller 
+public class UserController {
+	private List<User> users=new ArrayList<User>();
+	
+
+//	@GetMapping("/create")
+	@PostMapping("/create")
+//	public String create(String userid,String password,String name) {
+	public String create(User user) {
+//		System.out.println("uid : "+userid+", pw : "+password+", name : "+name);
+		System.out.println("user : "+user);
+		users.add(user);
+		return "redirect:list";
+	}
+	@GetMapping("/list")
+	public String list(Model model) {
+		System.out.println("list()");
+		model.addAttribute("users", users);
+		return "list";
+	}
+	
+	
+}
--- src/main/java/net/myslipp/web/WelcomeController.java
+++ src/main/java/net/myslipp/web/WelcomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 @Controller
 public class WelcomeController {
 	
@@ -22,13 +25,36 @@ public class WelcomeController {
 	@RequestMapping("view1")
 	public String view1(String name,Model model) {
 		System.out.println("name : "+name);
+		JSONArray jsonArray= new JSONArray();
+		JSONObject jsonObj=new JSONObject();
+		jsonObj.put("tag", "Tom");
+		jsonObj.put("flag", true);
+		
+		
+		
 		model.addAttribute("name", name);
+		model.addAttribute("json", jsonObj);
 		return "view1";
 	}
 	
 	@RequestMapping("view2")
 	public String view2(String name,Model model) {
 		System.out.println("name : "+name);
+		JSONArray jsonArray= new JSONArray();
+		JSONObject jsonObj1=new JSONObject();
+		jsonObj1.put("tag", "Tom");
+		jsonObj1.put("flag", true);
+		
+		JSONObject jsonObj2=new JSONObject();
+		jsonObj2.put("tag", "John");
+		jsonObj2.put("flag", false);
+		
+		
+		jsonArray.add(jsonObj1);
+		jsonArray.add(jsonObj2);
+		
+		
+		model.addAttribute("jsons", jsonArray);
 		model.addAttribute("name", name);
 		return "view2";
 	}
